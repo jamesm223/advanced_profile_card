c4e20b1049aca6c64c16d4f9c92d967cdb698ac1
James Morton <jamesmorton@Jamess-MBP.localdomain>
2025-05-08 10:10:49 +0100

    completed the task with all the necessary html, css and js to give it all the necessary functionality

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ function myFunction() {
     
 }
 
-function aboutFunction() {
-    var element = document.getElementById('bottom-text');
-    element.classList.toggle("flipped");
+
+function portfolioFunction() {
+    var element = document.getElementById('about-text')
+    var image = document.getElementById('profile-img')
+    var toptxt = document.getElementById('name')
+    element.classList.add('hidden')
+    image.classList.add('shrunk')
+    toptxt.classList.add('moved')
 }
 
-
+function aboutFunction() {
+    var element = document.getElementById('about-text')
+    var image = document.getElementById('profile-img')
+    var toptxt = document.getElementById('name')
+    element.classList.remove('hidden')
+    image.classList.remove('shrunk')
+    toptxt.classList.remove('moved')
+}
